3b6dd357ae36039bbbd4cc36482f0831f1d48059
YonHoshihara <hoshiharyon@gmail.com>
2022-10-29 22:55:26 -0300

    Add spring mechanic

---
+++ Assets/Scripts/Spring.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+
+    [SerializeField]
+    private float m_JumpForce;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals(GameDefines.m_PlayerTag))
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().CallJump(m_JumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
