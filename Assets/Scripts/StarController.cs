51547633fa30ded441a0255ca2cfa78e80845db8
YonHoshihara <hoshiharyon@gmail.com>
2022-11-15 18:12:52 -0300

    Add win Screen

--- Assets/Scripts/StarController.cs
+++ Assets/Scripts/StarController.cs
@@ -1,51 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] m_Starts;
-    
+    private Sprite[] m_Starts;
+
     [SerializeField]
+    private Image m_WinScreen;
+
     private GameObject[] m_Food;
+
 
     private void OnEnable()
     {
         m_Food = GameObject.FindGameObjectsWithTag(GameDefines.m_FoodTag);
         EnableStars(m_Food.Length);
     }
-
-    private void OnDisable()
-    {
-        DisableStars();
-    }
     private void EnableStars(int foodCount)
     {
         if (foodCount == 0)
         {
-            m_Starts[0].SetActive(true);
-            m_Starts[1].SetActive(true);
-            m_Starts[2].SetActive(true);
+            m_WinScreen.sprite = m_Starts[2];
         }
 
         if (foodCount == 1)
         {
-            m_Starts[0].SetActive(true);
-            m_Starts[1].SetActive(true);
+            m_WinScreen.sprite = m_Starts[1];
         }
 
         if (foodCount == 2)
         {
-            m_Starts[0].SetActive(true);
+            m_WinScreen.sprite = m_Starts[0];
         }
     }
 
-    private void DisableStars()
-    {
-        for (int i = 0; i <= m_Starts.Length - 1; i++)
-        {
-            m_Starts[i].SetActive(false);
-        }
-    } 
 }
