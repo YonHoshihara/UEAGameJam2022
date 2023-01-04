726eb88ed259bbf3d04d9957f83994aa13a600a4
YonHoshihara <hoshiharyon@gmail.com>
2023-01-03 23:11:56 -0400

    Update player Input System to new Input System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -20,29 +23,46 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 m_MovementVector;
     
     private bool m_IsJumping;
-    
+
+    private PlayerControlls m_Controls;
     // Start is called before the first frame update
     void Awake()
     {
         m_RigidBody = GetComponent<Rigidbody2D>();
+        m_Controls = new PlayerControlls();
+        m_Controls.Gameplay.Jump.performed += ctx => JumpListener();
+        m_Controls.Gameplay.MoveInPad.performed += ctx => m_MovementVector = new Vector2(ctx.ReadValue<float>(),0);
+        m_Controls.Gameplay.MoveInPad.canceled += ctx => m_MovementVector = Vector2.zero;
+        m_Controls.Gameplay.LeftArrow.performed += ctx => m_MovementVector = new Vector2(-1,0);
+        m_Controls.Gameplay.LeftArrow.canceled += ctx => m_MovementVector = Vector2.zero;
+        m_Controls.Gameplay.RightArrow.performed += ctx => m_MovementVector = new Vector2(1,0);
+        m_Controls.Gameplay.RightArrow.canceled += ctx => m_MovementVector = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        m_Controls.Gameplay.Enable();
+    }
+
+    private void OnDisable()
+    {
+        m_Controls.Gameplay.Disable();
     }
 
     // Update is called once per frame
     void Update()
-    {     
-        m_MovementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
-        Jump(m_JumpForce);
+    {
         if (m_IsJumping)
         {
             m_Animator.SetFloat("YVelocity", m_RigidBody.velocity.y);
         }
     }
-
+    
     void FixedUpdate()
     {
         Move(m_MovementVector,m_MovementSpeed);
     }
-
+    
     private void Move(Vector3  movement , float speed)
     {
         if (!LevelController.m_Instance.GameOverStatus())
@@ -71,7 +91,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump(float jumpforce)
     {
-        if (Input.GetKeyDown(GameDefines.m_JumpButton) && !m_IsJumping)
+        if (!m_IsJumping)
         {
             m_Animator.SetTrigger("Jump");
             SoundController.Instance.PlaySound(5);
@@ -80,7 +100,7 @@ public class PlayerMovement : MonoBehaviour
             CallJump(jumpforce, ForceMode2D.Impulse);
         }
     }
-
+    
     public void CallJump(float jumpforce, ForceMode2D forcemode)
     {
         m_RigidBody.AddForce(new Vector2(0, jumpforce), forcemode);
@@ -132,4 +152,14 @@ public class PlayerMovement : MonoBehaviour
 
         return GameDefines.PlayerDirection.LEFT;
     }
+
+
+    #region InputListeners
+
+    private void JumpListener()
+    {
+        Jump(m_JumpForce);
+    }
+    
+    #endregion
 }
